701bb376ff7624546c0405119b19c1243547e617
Eduardo <eduardomell06@hotmail.com>
2024-10-06 15:38:22 -0300

    [refactor]: apply dictionary on menu

--- lib/app.rb
+++ lib/app.rb
@@ -6,17 +6,10 @@ class App
  
   def self.call
 
-    # Product.new(name: "Funko", price: 50)
-    # Product.new(name: "Funko2", price: 500)
-    # Product.new(name: "Funko3", price: 100)
+    Product.new(name: "Funko", price: 50)
+    Product.new(name: "Funko2", price: 500)
+    Product.new(name: "Funko3", price: 100)
 
-    # Product.all.first.as_json
-    # # {name: "funko", price: 50}
-    # binding.irb
-    # Product.search(5)
-    # Product.search('o')
-
-    # # menu
     Menu.call
     "running"
   end
--- lib/models/menu.rb
+++ lib/models/menu.rb
@@ -1,12 +1,11 @@
 class Menu
   def initialize
-    @options = [
-      'exit',
-      'new',
-      'all',
-      'search'
-    ]
-    @continue = true
+    @options = {
+      0 => { label: 'exit', action: -> {} },
+      1 => { label: 'new', action: -> { new } },
+      2 => { label: 'all', action: -> { all } },
+      3 => { label: 'search', action: -> { search } }
+    }
   end
 
   def self.call
@@ -20,32 +19,54 @@ class Menu
   private
 
   def run 
-    while @continue
-      system('clear')
-      puts "MENU"
-      puts "Escolha a opção desejada"
-      @options.each_with_index { |option, index| puts "#{index}: #{option}"}
-      option = gets.chomp.to_i
+    while 
+      show_options
+      choose_option
+      run_option
 
-      case option
-        when 1
-          puts "Name of product: "
-          name = gets.chomp
-          puts "Price of product: "
-          price = gets.chomp.to_i
-          Product.new(name: name, price: price)
-        when 2
-          products = Product.all
-          products.each { |product| puts product.as_json}
-        when 3
-          puts "Search: "
-          search = gets.chomp
-          products = Product.search(search)
-          products.each { |product| puts product.as_json}
-      end
-      @continue = false if option.zero?
-      puts "Press any key to continue..."
-      gets
+      check_exit
     end
   end
+
+  def show_options
+    system('clear')
+    puts "MENU"
+    puts "Escolha a opção desejada"
+    @options.each { |option| puts "#{option.first} - #{option.last[:label]}"}
+  end
+
+  def choose_option
+    @option = gets.chomp.to_i
+  end
+
+  def run_option
+    @options[@option][:action].call
+  end
+
+  def check_exit
+    exit 0 if @option.zero?
+    puts "Press any key to continue..."
+    gets
+  end
+
+  def all
+    products = Product.all
+    products.each { |product| puts product.as_json}
+  end
+
+  def new
+    puts "Name of product: "
+    name = gets.chomp
+    puts "Price of product: "
+    price = gets.chomp.to_i
+    Product.new(name: name, price: price)
+  end
+
+  def search
+    puts "Search: "
+    search = gets.chomp
+    products = Product.search(search)
+    products.each { |product| puts product.as_json}
+  end
+
 end
